026526728f4f91a16d04966f269c97117be9ea48
Ulysses Souza <ulyssessouza@gmail.com>
2020-02-21 11:00:57 +0100

    Add tests for volumes, secrets and configs
    - Volumes
    - Secrets
    - Configs are implemented but disabled because docker-compose
    does not support that yet
    
    Signed-off-by: Ulysses Souza <ulyssessouza@gmail.com>

--- compliance_test.go
+++ compliance_test.go
@@ -8,9 +8,12 @@ import (
 )
 
 const (
-	defaultPingEntrypoint = "http://localhost:8080/ping"
-	targetUrl             = "target:8080/ping"
-	defaultTargetUrl      = defaultPingEntrypoint + "?address=" + targetUrl
+	pingEntrypoint = "http://localhost:8080/ping"
+	pingTargetUrl  = "target:8080/ping"
+	pingUrl        = pingEntrypoint + "?address=" + pingTargetUrl
+
+	volumefileEntrypoint = "http://localhost:8080/volumefile"
+	volumeUrl            = volumefileEntrypoint + "?filename="
 )
 
 func TestSimpleLifecycle(t *testing.T) {
@@ -23,7 +26,7 @@ func TestSimpleLifecycle(t *testing.T) {
 func TestSimpleNetwork(t *testing.T) {
 	h := TestHelper{T: t, testDir: "simple_network"}
 	h.testUpDown(func(t *testing.T) {
-		actual := getHttpBody(t, defaultTargetUrl)
+		actual := getHttpBody(t, pingUrl)
 		assert.Assert(t, actual == "{\"response\":\"PONG FROM TARGET\"}\n")
 	})
 }
@@ -39,7 +42,32 @@ func TestSimpleNetworkFail(t *testing.T) {
 func TestDifferentNetworks(t *testing.T) {
 	h := TestHelper{T: t, testDir: "different_networks"}
 	h.testUpDown(func(t *testing.T) {
-		actual := getHttpBody(t, defaultTargetUrl)
+		actual := getHttpBody(t, pingUrl)
 		assert.Assert(t, actual == "{\"response\":\"Could not reach address: target:8080/ping\"}\n")
+	})
+}
+
+func TestVolumeFile(t *testing.T) {
+	h := TestHelper{T: t, testDir: "simple_volume"}
+	h.testUpDown(func(t *testing.T) {
+		actual := getHttpBody(t, volumeUrl+"test_volume.txt")
+		assert.Assert(t, actual == "{\"response\":\"MYVOLUME\"}\n")
+	})
+}
+
+func TestSecretFile(t *testing.T) {
+	h := TestHelper{T: t, testDir: "simple_secretfile"}
+	h.testUpDown(func(t *testing.T) {
+		actual := getHttpBody(t, volumeUrl+"test_secret.txt")
+		assert.Assert(t, actual == "{\"response\":\"MYSECRET\"}\n")
+	})
+}
+
+// Ignored because docker-compose does not support that for now
+func _TestConfigFile(t *testing.T) {
+	h := TestHelper{T: t, testDir: "simple_configfile"}
+	h.testUpDown(func(t *testing.T) {
+		actual := getHttpBody(t, volumeUrl+"test_config.txt")
+		assert.Assert(t, actual == "{\"response\":\"MYCONFIG\"}\n")
 	})
 }
